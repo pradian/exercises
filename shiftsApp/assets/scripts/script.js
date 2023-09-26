59b87a4b0b9a965d67e0fe63928efa39752d797c
pradian <alexandru.petre@live.com>
2023-09-26 19:46:57 +0300

    1

--- shiftsApp/assets/scripts/script.js
+++ shiftsApp/assets/scripts/script.js
@@ -13,3 +13,49 @@ homeLoginBtn.addEventListener("click", () => {
   homepage.classList.toggle("hidden");
   loginForm.classList.toggle("hidden");
 });
+
+const registrationUsername = document.getElementById("registrationUsername");
+const registrationFirstName = document.getElementById("registrationFirstName");
+const registrationLastName = document.getElementById("registrationLastName");
+const registrationEmail = document.getElementById("registrationEmail");
+const registrationAge = document.getElementById("registrationAge");
+const registrationPassword = document.getElementById("registrationPassword");
+const registrationConfirmPassword = document.getElementById(
+  "registrationConfirmPassword"
+);
+const registrationBtn = document.getElementById("registrationBtn");
+const registrationClearBtn = document.getElementById("registrationClearBtn");
+
+class User {
+  //   id: 0;
+  constructor(username, firstName, lastName, email, age, password) {
+    this.username = username;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.age = age;
+    this.password = password;
+  }
+}
+
+registrationClearBtn.addEventListener("click", () => {
+  registrationUsername.value = "";
+  registrationFirstName.value = "";
+  registrationLastName.value = "";
+  registrationEmail.value = "";
+  registrationAge.value = "";
+  registrationPassword.value = "";
+  registrationConfirmPassword.value = "";
+});
+
+registrationBtn.addEventListener("click", () => {
+  let user = new User(
+    registrationUsername.value,
+    registrationFirstName.value,
+    registrationLastName.value,
+    registrationEmail.value,
+    registrationAge.value,
+    registrationPassword.value
+  );
+  console.log(user);
+});
