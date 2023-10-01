a194d1ca258bc5a7ad6de46a40bff80a2a707e13
pradian <alexandru.petre@live.com>
2023-10-01 20:43:12 +0300

    end for today, Small adjustments

--- shiftsApp/assets/scripts/home.js
+++ shiftsApp/assets/scripts/home.js
@@ -27,7 +27,6 @@ const logoutBtn = document.getElementById("logoutBtn");
 logoutBtn.addEventListener("click", () => {
   localStorage.removeItem("loggedIn");
   window.location.replace("index.html");
-  console.log("Logged out");
 });
 
 // User profile button
@@ -39,11 +38,29 @@ editProfile.addEventListener("click", () => {
 const showShifts = document.querySelector("tbody");
 showShifts.innerHTML = "";
 loggedInUser.shifts.forEach((shift) => {
+  const startTime = new Date(shift.startTime);
+  const endTime = new Date(shift.endTime);
+
+  const formattedStartTime = `${startTime.toLocaleDateString("en-US", {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  })}, ${startTime.getHours()}:${String(startTime.getMinutes()).padStart(
+    2,
+    "0"
+  )}`;
+  const formattedEndTime = `${endTime.toLocaleDateString("en-US", {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  })}, ${endTime.getHours()}:${String(endTime.getMinutes()).padStart(2, "0")}`;
   const row = document.createElement("tr");
   row.innerHTML = `
   <td>${shift.name}</td>
-  <td>${shift.startTime}</td>
-  <td>${shift.endTime}</td>
+  <td>${formattedStartTime}</td>
+  <td>${formattedEndTime}</td>
   <td>${
     (new Date(shift.endTime) - new Date(shift.startTime)) / 1000 / 60 / 60
   }</td>
@@ -53,10 +70,58 @@ loggedInUser.shifts.forEach((shift) => {
 `;
   showShifts.appendChild(row);
 });
-console.log(showShifts);
-const bestMonth = document.getElementById("bestMonth");
 
-const bestMonthShifts = loggedInUser.shifts.filter((shift) => {
-  shift.date.split("-")[1];
-  return shift.date.split("-")[1] === "05";
-});
+// // Calculate best month
+// const showBestMonth = document.querySelector("tfoot");
+
+// function calculateTotalPerMonth(shifts, year, month) {
+//   return shifts.reduce((total, shift) => {
+//     const shiftDate = new Date(shift.endTime);
+//     if (shiftDate.getFullYear() === year && shiftDate.getMonth() === month) {
+//       return total + shift.total;
+//     }
+//     return {total: total };
+//   }, 0);
+// }
+// function calculateBestMonth(shifts) {
+//   const bestMonth = {
+//     month: 0,
+//     year: 0,
+//     total: 0,
+//   };
+//   for (let i = 0; i < 12; i++) {
+//     const total = calculateTotalPerMonth(shifts, new Date().getFullYear(), i);
+//     const monthName = [
+//       "January",
+//       "February",
+//       "March",
+//       "April",
+//       "May",
+//       "June",
+//       "July",
+//       "August",
+//       "September",
+//       "October",
+//       "November",
+//       "December",
+//     ];
+//     if (total > bestMonth.total) {
+//       bestMonth.month = monthName[i];
+//       bestMonth.year = new Date().getFullYear();
+//       bestMonth.total = total;
+//     }
+//   }
+//   return {
+//     month: bestMonth.month,
+//     year: bestMonth.year,
+//     total: bestMonth.total,
+//   };
+// }
+
+// const tFootRow = document.createElement("tr");
+
+// const bestMonth = calculateBestMonth(loggedInUser.shifts);
+
+// tFootRow.innerHTML = `<th colspan="3">Best month:</th> <th colspan="2"> ${bestMonth.month} ${bestMonth.year}</th> <th colspan="2"> ${bestMonth.total}$</th>`;
+
+// showBestMonth.appendChild(tFootRow);
